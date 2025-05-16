2624803572df2beac656e5c82a0696994c92b6d2
MarcosViniciusM <marcos.vinicius.ferreira.700@ufrn.edu.br>
2025-05-16 13:27:16 -0300

    cleanup banco.h

--- banco.h
+++ banco.h
@@ -2,27 +2,27 @@
 using namespace std;
 
 class Cliente {
-    string nome, cpf;
+    	string nome, cpf;
 public:
-    Cliente();
-    Cliente(string, string);
-    string getNome();
+    	Cliente();
+    	Cliente(string, string);
+    	string getNome();
 	void setNome(string n);
-    string getCPF();
-    void setCPF(string c);
+    	string getCPF();
+    	void setCPF(string c);
 };
 
 class ContaBancaria {
-    int numero;
-    double saldo;
-    Cliente titular;
+    	int numero;
+    	double saldo;
+    	Cliente titular;
 public:
-    ContaBancaria(int, Cliente);
-    ContaBancaria(int, Cliente, double);
-    void depositar(double valor);
-    void sacar(double valor);
-    void transferir(double valor, ContaBancaria &destino);
-    void transferir(double valor, ContaBancaria &destino1, ContaBancaria &destino2);
-    void exibirSaldo();
-    void exibirInformacoes();
+	ContaBancaria(int, Cliente);
+    	ContaBancaria(int, Cliente, double);
+    	void depositar(double valor);
+    	void sacar(double valor);
+    	void transferir(double valor, ContaBancaria &destino);
+   	void transferir(double valor, ContaBancaria &destino1, ContaBancaria &destino2);
+ 	void exibirSaldo();
+	void exibirInformacoes();
 };
