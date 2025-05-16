86436784a8fd5758dd931892050c9f6e1bb9ba31
MarcosViniciusM <marcos.vinicius.ferreira.700@ufrn.edu.br>
2025-05-16 13:25:55 -0300

    cleanup banco.cpp

--- banco.cpp
+++ banco.cpp
@@ -5,8 +5,8 @@ using namespace std;
 Cliente::Cliente(){}
 
 Cliente::Cliente(string n, string c){
-    nome = n;
-    cpf = c;
+	nome = n;
+	cpf = c;
 }
 
 string Cliente::getNome(){
@@ -26,65 +26,65 @@ void Cliente::setCPF(string c){
 }
 
 ContaBancaria::ContaBancaria(int n, Cliente t){
-    numero = n;
-    titular.setNome(t.getNome());
-    titular.setCPF(t.getCPF());
-    saldo=0;
+	numero = n;
+	titular.setNome(t.getNome());
+    	titular.setCPF(t.getCPF());
+    	saldo=0;
 }
 
 ContaBancaria::ContaBancaria(int n, Cliente t, double s){
-    numero = n;
-    titular.setNome(t.getNome());
-    titular.setCPF(t.getCPF());
-    saldo = s;
+	numero = n;
+	titular.setNome(t.getNome());
+	titular.setCPF(t.getCPF());
+	saldo = s;
 }
 
 void ContaBancaria::depositar(double valor){
 	// Impede a operação se for um valor negativo
 	if(valor<0){
-        return;
-    }
-    saldo+=valor;
+            return;
+	}
+	saldo+=valor;
 }
 
 void ContaBancaria::sacar(double valor){
 	// Impede a operação se for um valor negativo
-    if (valor<0){
-        return;
-    }
+	if (valor<0){
+        	return;
+	}
 	// Impede a operação se o valor for maior do que o saldo disponível
-    if(valor>saldo){
-        return;
-    }
-    saldo-=valor;
+    	if(valor>saldo){
+        	return;
+    	}
+    	saldo-=valor;
 }
 
 void ContaBancaria::transferir(double valor, ContaBancaria &destino){
 	// Impede a operação se o valor for maior do que o saldo disponível
-    if(valor>saldo){
-        return;
-    }
+    	if(valor>saldo){
+        	return;
+    	}
 	// Saca o valor da conta original, e depois deposita na conta alvo
-    sacar(valor);
-    destino.depositar(valor);
+    	sacar(valor);
+    	destino.depositar(valor);
 }
 
 void ContaBancaria::transferir(double valor, ContaBancaria &destino1, ContaBancaria &destino2){
 	// Impede a operação se o valor for maior do que o saldo disponível
-    if(valor>saldo){
-        return;
-    }
+    	if(valor>saldo){
+        	return;
+    	}
 	// Saca o valor da conta original, e depois deposita nas duas contas alvo
-    sacar(valor);
-    destino1.depositar(valor/2);
-    destino2.depositar(valor/2);
+    	sacar(valor);
+    	destino1.depositar(valor/2);
+    	destino2.depositar(valor/2);
 }
 
 void ContaBancaria::exibirSaldo(){
-    cout << "Saldo atual da conta " << numero << ": R$ " << saldo << endl;
+	cout << "Saldo atual da conta " << numero << ": R$ " << saldo << endl;
 }
 
 void ContaBancaria::exibirInformacoes(){
-    cout << "Titular: " << titular.getNome() << ", CPF: " << titular.getCPF() << endl;
-    cout << "Numero da conta: " << numero << ", Saldo : R$ " << saldo << endl;
+	cout << "Titular: " << titular.getNome() << ", CPF: " << titular.getCPF() << endl;
+	cout << "Numero da conta: " << numero << ", Saldo : R$ " << saldo << endl;
 }
